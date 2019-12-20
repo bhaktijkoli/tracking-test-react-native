f43cc2615d386e0de7a7a243170ffc850aedf597
Bhaktij Koli <bhaktijkoli121@gmail.com>
2019-12-20 05:45:19 +0530

    send location to tcp server

--- client.js
+++ client.js
@@ -1,4 +1,5 @@
 import TcpSocket from 'react-native-tcp-socket';
+import Geolocation from 'react-native-geolocation-service';
 let host = '192.168.0.105';
 let port = 7070;
 let client = null;
@@ -6,6 +7,7 @@ let client = null;
 module.exports.connect = () => {
   client = TcpSocket.createConnection({host, port}, () => {
     console.log("Client connected");
+    module.exports.sendLocation();
     client.on('data', function(data) {
       console.log('message was received', data);
     });
@@ -18,5 +20,20 @@ module.exports.connect = () => {
       console.log('Connection closed!');
     });
   });
+}
 
+module.exports.sendLocation = () => {
+  Geolocation.getCurrentPosition(
+    (position) => {
+      if(client) {
+        let { latitude, longitude } = position.coords;
+        client.write(`${latitude}, ${longitude}`)
+      }
+    },
+    (error) => {
+      // See error code charts below.
+      console.log(error.code, error.message);
+    },
+    { enableHighAccuracy: true, timeout: 15000, maximumAge: 10000 }
+  );
 }
